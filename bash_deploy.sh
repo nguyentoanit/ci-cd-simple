7da9ff9f33e5676d93f24dd58a9da234c1b6b855
Fuji_Toan <toan.nguyen@fujitechjsc.com>
2018-03-08 16:08:12 +0700

    Update message on bash deploy file

--- bash_deploy.sh
+++ bash_deploy.sh
@@ -4,12 +4,12 @@
 echo '========================================='
 echo 'Deployment Logs:'
 
-echo '------------Development Server--------------'
+echo '\n\n------------Development Server--------------'
 cd /path/to/project
 git pull <repository> <branch>
 # Example git pull origin master
 
 #Migrate development database
-echo '------------Development Server Database Migration--------------'
+echo '\n\n------------Development Server Database Migration--------------'
 /path/to/flyway/flyway -url=jdbc:postgresql://<database-ip-address>:<port>/<database-name> -locations=filesystem:<path-to-sql-folder> migrate
 # Example: /path/to/flyway/flyway -url=jdbc:postgresql://172.0.0.1:5432/database1 -locations=filesystem:/var/www/project/public_html/database migrate
